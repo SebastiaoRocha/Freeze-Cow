c255a8741a228cd9a2e52a69ae473c3425f747e4
zilliana123 <zilliana.veg@gmail.com>
2025-06-21 12:16:36 -0300

    feat: model fornecedor

--- src/models/fornecedorModel.js
+++ src/models/fornecedorModel.js
@@ -1,1 +1,32 @@
-//**** */
+const { sequelize } = require('../config/db'); 
+const { DataTypes } = require('sequelize');
+
+const fornecedorModel = sequelize.define("Fornecedores", {
+    ID_Fornecedor:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    nomeFornecedor:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cnpjFornecedor:{
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    enderecoFornecedor:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    telefoneFornecedor:{
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+},{
+    tableName: 'Fornecedores',
+    timestamps: false
+});
+
+module.exports = {fornecedorModel};
