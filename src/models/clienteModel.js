4cdf6c99a0f4bc7defd0992f30396a75d1bd0e10
unknown <studyworkdinha@gmail.com>
2025-06-21 16:03:22 -0300

    Model do Cliente

---
+++ src/models/clienteModel.js
@@ -0,0 +1,44 @@
+const { sequelize } = require('../config/bd');
+const { DataTypes} = require('sequelize');
+
+const { unidadeModel } = require('./unidadeModel');
+
+const clienteModel = sequelize.define('Clientes', {
+    ID_Cliente:{
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    ID_FK_Unidade:{
+        type: DataTypes.INTEGER,
+        references:{
+            model: unidadeModel,
+            key: 'ID_Unidade'
+        },
+        allowNull: false
+    },
+    nome_Cliente:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    cpf_Cliente:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    endereco_Cliente:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    telefone_Cliente:{
+        type: DataTypes.STRING,
+        allowNull: true
+    }
+},{
+    tableName: 'Clientes',
+    timestamps: false
+});
+
+unidadeModel.hasMany(clienteModel, {foreignKey:'ID_FK_Unidade', as: 'Unidades'}); 
+clienteModel.belongsTo(unidadeModel, {foreignKey:'ID_FK_Unidade', as: 'Unidades'});
+
+module.exports = {clienteModel};
